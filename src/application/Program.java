331dcbe0b14807d4c4e188bf31f573d074e58bf3
Luiz Fernando de Morais <luiz.ado@hotmail.com>
2019-03-21 23:15:15 -0300

    Version with path question

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,42 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<String> list = new ArrayList<>();
+
+		
+		System.out.println("Enter the file path: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String name = br.readLine();
+			while (name != null) {
+				list.add(name);
+				name = br.readLine();
+			}
+			Collections.sort(list);
+			for (String s : list) {
+				System.out.println(s);
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
 	}
 
 }
